2af076fc8defa05dbf16ee6342c4156b85d552c0
Dasha2221 <zaricnadarina@gmail.com>
2023-10-15 15:41:46 +0300

    slider-header

--- src/route/index.js
+++ src/route/index.js
@@ -4,10 +4,12 @@ const router = express.Router()
 
 // Підключіть файли роутів
 const test = require('./test')
+const slider = require('./slider')
 // Підключіть інші файли роутів, якщо є
 
 // Об'єднайте файли роутів за потреби
 router.use('/', test)
+router.use('/slider', slider)
 // Використовуйте інші файли роутів, якщо є
 
 // Експортуємо глобальний роутер
